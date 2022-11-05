f0c83430d9c1caa5eb3f4284364a8fd218375e10
Dalton1199 <83455276+Dalton1199@users.noreply.github.com>
2022-11-05 01:54:30 -0600

    add leader line

--- src/routes/plan-estudios/+page.svelte
+++ src/routes/plan-estudios/+page.svelte
@@ -1,99 +1,21 @@
 <script>
     import { onMount } from 'svelte';
 
-    function adjustLine(from, to, line){
+    let start, end;
 
-        var fT = from.offsetTop  + from.offsetHeight/2;
-        var tT = to.offsetTop    + to.offsetHeight/2;
-        var fL = from.offsetLeft + from.offsetWidth/2;
-        var tL = to.offsetLeft   + to.offsetWidth/2;
-
-        var CA   = Math.abs(tT - fT);
-        var CO   = Math.abs(tL - fL);
-        var H    = Math.sqrt(CA*CA + CO*CO);
-        var ANG  = 180 / Math.PI * Math.acos( CA/H );
-
-        if(tT > fT){
-            var top  = (tT-fT)/2 + fT;
-        }else{
-            var top  = (fT-tT)/2 + tT;
-        }
-        if(tL > fL){
-            var left = (tL-fL)/2 + fL;
-        }else{
-            var left = (fL-tL)/2 + tL;
-        }
-
-        if(( fT < tT && fL < tL) || ( tT < fT && tL < fL) || (fT > tT && fL > tL) || (tT > fT && tL > fL)){
-            ANG *= -1;
-        }
-        top-= H/2;
-
-        line.style["-webkit-transform"] = 'rotate('+ ANG +'deg)';
-        line.style["-moz-transform"] = 'rotate('+ ANG +'deg)';
-        line.style["-ms-transform"] = 'rotate('+ ANG +'deg)';
-        line.style["-o-transform"] = 'rotate('+ ANG +'deg)';
-        line.style["-transform"] = 'rotate('+ ANG +'deg)';
-        line.style.top    = top+'px';
-        line.style.left   = left+'px';
-        line.style.height = H + 'px';
-    }
     onMount(async () => {
-        adjustLine(
-            document.getElementById('div1'), 
-            document.getElementById('div2'),
-            document.getElementById('line')
-        );
-   
+        const {LeaderLine} = await import('../../utils/LeaderLine/leader-line.min.js')
+        new LeaderLine(start, end, {color: '#f4f4f4', size: 8});
+
     });
-    
+
+
 </script>
 
 <div class="container m-auto">
     
-    <div id="content">
-        <div id="line"></div>
-        <div id="div1" class="mydiv"></div>
-        <div id="div2" class="mydiv"></div>
+    <div class="flex justify-between flex-row">
+        <div bind:this={start} class="w-80 bg-blue-900 h-80 mt-56"></div>
+        <div bind:this={end} class="w-80 bg-red-500 h-80"></div>
     </div>
 </div>
-
-<style>
-    #content{
-  position:relative;
-}
-.mydiv{
-  border:1px solid #368ABB;
-  background-color:#43A4DC;
-  position:absolute;
-}
-.mydiv:after{
-  content:no-close-quote;
-  position:absolute;
-  top:50%;
-  left:50%;
-  background-color:black;
-  width:4px;
-  height:4px;
-  border-radius:50%;
-  margin-left:-2px;
-  margin-top:-2px;
-}
-#div1{
-  left:0px;
-  top:0px;
-  width:100px;
-  height:100px;
-}
-#div2{
-  left:200px;
-  top:900px;
-  width:50px;
-  height:40px;
-}
-#line{
-  position:absolute;
-  width:1px;
-  background-color:red;
-}  
-</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import legacy from '@vitejs/plugin-legacy'
 
 const config = {
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		legacy({ 'node_modules/leader-line/leader-line.min.js': 'LeaderLine' }),
+	]
 };
 
 export default config;
